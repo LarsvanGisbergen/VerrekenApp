7a8118a3b2a07382e5da49028e25b21fff38e663
Lars <larsvangisbergen@gmail.com>
2021-06-25 19:11:49 +0200

    Experimenteren met components en services.

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpenseFormComponent } from './expense-form/expense-form.component';
+import { SummaryComponent } from './summary/summary.component';
+import { MatInputModule } from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpenseFormComponent,
+    SummaryComponent   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatInputModule   
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {    
+}
---
+++ my-app/src/app/monetary/monetary.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class MonetaryService {
+
+  bills: Bill[] = []
+  payments: Payment[] = []
+
+  constructor() { }
+
+  //returns true if bill was added succesfully
+  addBill(name: string, amount: number): boolean{
+    let bill:Bill = new Bill(name, amount);
+    try{this.bills.push(bill);}catch{return false;}    
+    return true;
+  }
+
+  getBills(): Observable<Bill[]> {
+    return of(this.bills);
+  }
+  
+  getPayments(): Observable<Payment[]> {
+    return of(this.payments);
+  }
+  
+}
+
+
+
+export class Bill{
+  name!: string;
+  amount!: number;
+
+  
+  constructor(name: string, amount: number) {
+    this.name = name;
+    this.amount = amount;   
+  }
+}
+
+export class Payment{
+  name!: string;
+  amount!: number;
+  name_receiver!: string;
+  
+  constructor(name: string, amount: number, name_receiver: string) {
+    this.name = name;
+    this.amount = amount;
+    this.name_receiver = name_receiver;
+  }
+}
